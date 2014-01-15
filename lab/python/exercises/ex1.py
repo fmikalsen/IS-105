e41683b3a8abd6078ae79ccc5befbffb38f9ac7e
Frode Mikalsen <dracz@dracz-MS-7680.(none)>
2014-01-15 11:53:50 +0100

    Lab 1

---
+++ lab/python/exercises/ex1.py
@@ -0,0 +1,9 @@
+
+
+
+# This basically only prints out the hardcoded text I've written.
+print "Hello World"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print "Yay! printing."
---
+++ lab/python/exercises/ex2.py
@@ -0,0 +1,8 @@
+#This is a comment it starts with #
+
+print "This is a print message"
+
+#print "This print message is inside a comment, and will therefore not run"
+
+print "This is so awesome"
+
