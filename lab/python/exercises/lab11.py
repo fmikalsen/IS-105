5c3ea892e5eb976593c58de2a96dce8510f3bfa7
Frode Mikalsen <frode@crunchbang.(none)>
2014-04-30 11:57:22 +0200

    lab11

--- lab/python/exercises/lab11.py
+++ lab/python/exercises/lab11.py
@@ -82,12 +82,20 @@ def hand_rank(hand):
 		return (8, max(ranks)) # 2 3 4 5 6 => (8, 6)
 	elif kind(4, ranks): # kan returnere både boolean og tall, i Java 0 er False
 		return (7, kind(4, ranks), kind(1, ranks)) # 9 9 9 9 3 (7, 9, 3)
+	elif kind(3, ranks) and kind(2, ranks):
+		return (6, kind(3, ranks), kind(2, ranks))
 	elif flush(hand):
 		return (5, ranks)
 	elif straight(ranks):
-		return (3, ranks)
+		return (4, ranks)
+	elif kind(4, ranks):
+		return (3, kind(3, ranks))
 	elif two_pair(ranks):
 		return (2, ranks)
+	elif pair(ranks):
+		return (1, ranks)
+	else:
+		return hand
 	
 	
 # Funksjonene card_ranks(hand) returnerer en ORDNET (sorted) tuple av verdier (ranks)
@@ -95,28 +103,21 @@ def hand_rank(hand):
 # En hånd TD TC TH 7C 7D skal returnere [10,10,10,7,7]
 def card_ranks(hand):
 	# Denne funksjonen ble implementert i LAB 5. Bruk implementasjonen her
-	if hand == []:
-		hand.split()
-
-	reps = {"T":"10","J":"11","Q":"12","K":"13","A":"14"} #dictionary for replacing letters with numbers
-	for char, numb in reps.iteritems():
-		hand = hand.replace(char, numb) #replaces the characters with "numbers"
-		r = re.findall(r"[0-9]+",hand) # removes the suit and r = "numbers"
-	r = [int(i) for i in r] #Makes the items in the tuple int type
-	sorted(r, reverse = True)
-	return r
-
+	ranks = ['--23456789TJQKA'.index(r) for r,s in hand]
+	ranks.sort(reverse=True)
+	return ranks
 
 # OPPGAVE: Implementer denne funksjonen
 # Funksjonen straight(ranks) returner True hvis hånden er en Straight.
 def straight(ranks):
 	return max(ranks) - min(ranks) == 4 and len(set(ranks)) == 5 # True if highest minus lowest number is 4 and its 5 unique cards
 
+def pair(ranks):
+	return len(set(ranks)) == 4
 
 # OPPGAVE: Implementere denne funksjonen
 # Funksjonen flush(hand) returnerer True hvis hånden er en Flush.
 def flush(hand):
-	s = hand.split()
 	types = [t[-1:] for t in s] #removes everthing but the last character (the suite)
 	return len(set(types)) == 1
 
@@ -124,7 +125,6 @@ def flush(hand):
 # Funksjonen kind(nr, ranks) returnerer den første verdien (rank) som hånden har nøyaktig n av.
 # For en hånd med 4 syvere, skal denne funksjonen returnere 7.
 def kind(nr, ranks):
-#	print ranks
 	for r in ranks:
 		if ranks.count(r) == nr: 
 			return r
@@ -174,11 +174,11 @@ def test():
 	assert max(lon1) == 8
 	assert max(lon2, key=abs) == -9
 
-	sf = "10C 9C 8C 7C 6C" #Straight Flush => ['6C', '7C', '8C', '9C', 'TC']
-	fk = "9D 9H 9S 9C 7D" # Four of a Kind
-	fh = "TD TC TH 7C 7D" # Full House
-	st = "3D 4C 5C 6H 7S" # Straight
-	tp = "8D 8C 4S 4H TD" # Two pairs
+	sf = "TC 9C 8C 7C 6C".split() #Straight Flush => ['6C', '7C', '8C', '9C', 'TC']
+	fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
+	fh = "TD TC TH 7C 7D".split() # Full House
+	st = "3D 4C 5C 6H 7S".split() # Straight
+	tp = "8D 8C 4S 4H TD".split() # Two pairs
 	assert poker([sf, fk, fh, st, tp]) == sf
 
 	# Disse testtilfellene ble skrevet i LAB 5
@@ -187,9 +187,9 @@ def test():
 	# 2) Full House (fh) mot Full House (fh) skal returnere Full House (fh)
 	# 3) Straight (st) skal slå Two pair (tp) OBS! Du må selv lage eksempler på hender her
 	
-#	assert poker([fk, fh]) == fk
-#	assert poker([fh, st]) == fh
-#	assert poker([st, tp]) == st
+	assert poker([fk, fh]) == fk
+	assert poker([fh, fh]) == fh
+	assert poker([st, tp]) == st
 			
 	# Dette ble gjort i LAB 5
 	# 1) teste et tilfelle der det kun er en hånd og at poker returnerer den samme hånden
@@ -197,8 +197,8 @@ def test():
 	# og det må da returnere Straight Flush (urealistisk med så mange spillere, men 
 	# vi tar høyde for det).
 	# Hva skjer hvis man har en tom liste som inn-data, dvs. ingen hender?
-	assert poker([sf]) == sf
-#	assert poker([sf, 100*fh]) == sf
+	assert poker([st]) == st
+	assert poker([sf, 100*fh]) == sf
 
 	# Dette ble gjort i LAB 5
 	# Implementer funksjonen card_rank(hand) og legg til tester for 
@@ -206,18 +206,16 @@ def test():
 	# Du kan gjerne definere flere hender og legge til flere tester :)
 	assert card_ranks(sf) == [10,9,8,7,6]
 	assert card_ranks(fk) == [9,9,9,9,7]
+	
 
 	# 
 	# hand_rank(..) implementere i denne laben (LAB 11)
 	# Her er gitt noen eksempler på testing av denne funksjonen som man kan bruke på et senere tidspunkt
 	#
-	#assert hand_rank(sf) == (8,10)
-	#assert hand_rank(fk) == (7,9,7)
-	#assert hand_rank(fh) == (6,10,7)
+	assert hand_rank(sf) == (8,10)
+	assert hand_rank(fk) == (7,9,7)
+	assert hand_rank(fh) == (6,10,7)
 	return "Done testing"
 
 print test()
-
-start = deal(1)
-print start
-print poker(start)
+print poker(deal(3))
